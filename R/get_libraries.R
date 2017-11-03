64d3a28499f6b0a91733b0849b7ceb59779fb27b
16EAGLE <jxsw@web.de>
2017-11-03 11:53:08 +0100

    windows issue searching for libraries

--- R/get_libraries.R
+++ R/get_libraries.R
@@ -72,7 +72,7 @@ get_libraries <- function(lib.tool = "all", dir = "auto", ...){
   }
   
   try.lib <- sapply(lib.tool, function(x){quiet(try(cmd.fun(x,ignore.stdout = TRUE,ignore.stderr = TRUE)))})
-  try.lib.which <- which(try.lib > 100)
+  try.lib.which <- which(try.lib >= 4)
   
   if(length(try.lib.which) == 0){lib.found <- lib.tool
   }else{lib.found <- lib.tool[-try.lib.which]}
