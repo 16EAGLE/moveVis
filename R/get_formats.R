ab321ea3ea5fc4ff48aa17566699659df55225f3
J. Schwalb-Willmann <jxsw@web.de>
2018-09-07 14:51:33 +0200

    adjusted get_formats

--- R/get_formats.R
+++ R/get_formats.R
@@ -29,30 +29,40 @@ get_formats <- function(tool = "auto"){
   
   detect_formats <- function(call){
     formats.supp <- sapply(lapply(cmd.fun(paste0(call," -formats"),intern = TRUE, ignore.stdout = FALSE, ignore.stderr = TRUE),
-                                function(x){substring(x,5,20)}), function(x){unlist(strsplit(x, " "))[1]})
+                                  function(x){substring(x,5,20)}), function(x){unlist(strsplit(x, " "))[1]})
     formats.supp <- formats.supp[5:length(formats.supp)]
     return(grep("gif", formats.supp, value = T, invert = T))
   }
   
   if(.Platform$OS.type == 'windows'){cmd.fun <- shell}else{cmd.fun <- system}
   
-  if(tool == "auto"){
-    command <- NULL
-    if(is.na(match("convert",get_libraries(log_level = 3))) == FALSE){command <- "convert"}
-    if(is.na(match("avconv",get_libraries(log_level = 3))) == FALSE){command <- "avconv"}
-    if(is.na(match("ffmpeg",get_libraries(log_level = 3))) == FALSE){command <- "ffmpeg"}
-    if(is.null(command)){
+  if(tool[1] == "auto"){
+    tool <- NULL
+    if(is.na(match("convert",get_libraries(log_level = 3))) == FALSE){tool <- c(tool, "convert")}
+    if(is.na(match("avconv",get_libraries(log_level = 3))) == FALSE){tool <-  c(tool, "avconv")}
+    if(is.na(match("ffmpeg",get_libraries(log_level = 3))) == FALSE){tool <-  c(tool, "ffmpeg")}
+    if(is.null(tool)){
       out("None of the necessary libraries could be detected on your system. Please run get_libraries() or install needed libraries manually (see manual).", type=3)
-    }else{
-      if(command == "convert"){df <- "gif"}else{df <- detect_formats(command)}
     }
   }else{
-    if(length(unlist(sapply(c("convert", "ffmpeg", "avconv"), function(x, t = tool) grep(x, t)))) > 0){
-      if(length(grep("convert", tool)) > 0) df <- "gif" else df <- detect_formats(tool)
-    }else{
+    if(length(unlist(sapply(c("convert", "ffmpeg", "avconv"), function(x, t = tool) grep(x, t)))) == 0){
       out(paste0("'",toString(tool),"' not recognized. 'tool' must eithr link to 'convert', 'ffmpeg' or 'avconv'."), type=3)
+    } else{
+      tool <- paste0('"', tool, '"')
     }
   }
+  
+  sub.conv <- grep("convert", tool)
+  if(length(sub.conv) > 0){
+    df <- "gif"
+    tool <- tool[-sub.conv]
+  }else{
+    df <- NULL
+  }
+  df <- c(df, sapply(tool, detect_formats, USE.NAMES = F))
+    
   vid <- c("gif", "mov", "mp4", "flv", "avi", "mpeg", "3gp", "ogg")
   return(vid[sort(as.numeric(na.omit(match(df,vid))))])
+}
+
 }
