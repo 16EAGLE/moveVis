f5207b05d82fa7e1d6571775d9968060abd07538
16eagle <jxsw@web.de>
2020-03-05 16:18:20 +0100

    test env

--- tests/testthat/test-internal.R
+++ tests/testthat/test-internal.R
@@ -1,8 +1,11 @@
 skip_on_cran()
 context("internal")
 
-#if("df2move" %in% which_tests){
 test_that("internal (labels)", {
   expect_length(expect_is(moveVis:::.x_labels(c(NA, -10,0,10,20,30)), "expression"), 5)
   expect_length(expect_is(moveVis:::.y_labels(c(NA, -10,0,10,20,30)), "expression"), 5)
+})
+
+test_that("internal (onLoad)", {
+  expect_invisible(moveVis:::.onLoad())
 })
--- tests/testthat/test-settings.R
+++ tests/testthat/test-settings.R
@@ -3,8 +3,8 @@ context("settings")
 
 test_that("use_multicore", {
   expect_output(use_multicore(verbose = T))
-  expect_output(use_multicore(n_cores = 2, verbose = T))
-  expect_equal(getOption("moveVis.n_cores"), 2)
+  expect_output(use_multicore(n_cores = parallel::detectCores(), verbose = T))
+  expect_equal(getOption("moveVis.n_cores"), parallel::detectCores())
   expect_output(use_multicore(n_cores = 100, verbose = T))
   expect_equal(getOption("moveVis.n_cores"), parallel::detectCores())
   expect_output(use_multicore(n_cores = -1, verbose = T))
@@ -19,8 +19,4 @@ test_that("use_disk", {
   expect_output(use_disk(frames_to_disk = TRUE, dir_frames = paste0(tempdir(), "/moveVis/xyz/abc"), n_memory_frames = NULL))
   expect_output(expect_warning(use_disk(frames_to_disk = TRUE, n_memory_frames = 2)))
   use_disk(frames_to_disk = FALSE, verbose = F)
-})
-
-test_that("onLoad", {
-  expect_invisible(moveVis:::.onLoad())
 })
