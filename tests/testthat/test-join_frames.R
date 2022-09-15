9466082d0748a9068c58718cc2fae257a4a6a2d3
16eagle <jxsw@web.de>
2022-09-15 17:56:24 +0200

    updated tests

--- tests/testthat/test-join_frames.R
+++ tests/testthat/test-join_frames.R
@@ -5,7 +5,7 @@ test_that("join_frames", {
   # correct calls
   f1 <- frames_spatial(m.aligned, r_grad, r_times, r_type = "gradient", verbose = F)[1:10]
   f2 <- frames_graph(m.aligned, r_grad, r_times, graph_type = "hist", verbose = F)[1:10]
-  expect_length(expect_is(join_frames(frames_lists = list(f1, f2), verbose = F), "list"), 10)
+  expect_length(expect_is(join_frames(frames_lists = list(f1, f2), verbose = F), "moveVis"), 10)
   
   # false calls
   expect_error(join_frames(frames_lists = list(f1[1:10], f2[1:20]), verbose = F)) # differing lengths
