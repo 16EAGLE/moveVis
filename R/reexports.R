de9f71405a8c95b83bfc5d3e4ab8eea91c9be573
16EAGLE <jxsw@web.de>
2025-08-06 10:07:03 +0200

    potential fix for reexport linking issue

--- R/reexports.R
+++ R/reexports.R
@@ -1,11 +1,33 @@
+
+#' Objects exported from other packages
+#'
+#' @description 
+#' 
+#' These objects are imported from other packages. Follow the links below to see their documentation.
+#' 
+#' \describe{
+#'   \item{magrittr}{\code{\link[magrittr]{\%>\%}}}
+#'   \item{basemaps}{\code{\link[basemaps]{get_maptypes}}, \code{\link[basemaps]{flush_cache}}}
+#' }
+#' 
+#' @name reexports  
+#' @keywords internal
+NULL
+
+#' @rdname reexports
+#' @usage NULL
 #' @importFrom magrittr %>%
 #' @export
 magrittr::`%>%`
 
+#' @rdname reexports
+#' @usage NULL
 #' @importFrom basemaps get_maptypes
 #' @export
 basemaps::get_maptypes
 
+#' @rdname reexports
+#' @usage NULL
 #' @importFrom basemaps flush_cache
 #' @export
 basemaps::flush_cache
